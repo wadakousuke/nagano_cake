fdb09056019436d32b9f3db95cad2f3c88c93347
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-04-06 13:40:06 +0000

    [Add] admins,customersテーブルのカラム追加

--- db/migrate/20220406131325_devise_create_admins.rb
+++ db/migrate/20220406131325_devise_create_admins.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
+class DeviseCreateAdmins < ActiveRecord::Migration[6.1]
   def change
-    create_table :customers do |t|
+    create_table :admins do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -34,22 +34,11 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
 
 
       t.timestamps null: false
-      t.string :last_name, :null => false
-      t.string :first_name, :null => false
-      t.string :last_name_kana, :null => false
-      t.string :first_name_kana, :null => false
-      t.string :postal_code, :null => false
-      t.string :address, :null => false
-      t.string :telephone_number, :null => false
-      t.boolean :is_active, :null => false, :default => 'TRUE'
-
-
-
     end
 
-    add_index :customers, :email,                unique: true
-    add_index :customers, :reset_password_token, unique: true
-    # add_index :customers, :confirmation_token,   unique: true
-    # add_index :customers, :unlock_token,         unique: true
+    add_index :admins, :email,                unique: true
+    add_index :admins, :reset_password_token, unique: true
+    # add_index :admins, :confirmation_token,   unique: true
+    # add_index :admins, :unlock_token,         unique: true
   end
 end
