12e2a172b8f0f3957b9ee598d45b3befb65cb472
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-04-09 03:54:53 +0000

    [Add] ジャンル一覧・追加画面の追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,23 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def genre_params
+    params.permit(:name)
   end
 end
