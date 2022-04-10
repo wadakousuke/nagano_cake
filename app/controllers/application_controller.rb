6506f44ce5509be27fa8e76bd024721aafc3dce7
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-04-10 14:45:23 +0000

    [Finish] 20220410

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+private
+  def cart_item_params
+      params.permit(:item_id, :amount)
+  end
 end
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -12,5 +12,13 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    @cart_items = Item.new(cart_item_params)
+    binding.pry
+    @cart_items.save
+    redirect_to cart_items_path
+  end
+private
+  def cart_item_params
+      params.permit(:item_id, :amount)
   end
 end
