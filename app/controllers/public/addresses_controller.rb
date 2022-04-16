3b1e797dd4e2b24da1ce299e6e17b250b865a8a7
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-04-16 02:39:41 +0000

    [Add] 配送先登録一覧編集画面の追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,27 +1,36 @@
 class Public::AddressesController < ApplicationController
   def index
     @address = Address.new
-    @addresses =  Address.all
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
-    @address = Address.new(params[:id])
+    @address = Address.new(address_params)
     @address.customer_id = current_customer.id
+    @address.save
+    redirect_to addresses_path
   end
 
   def update
+     @address = Address.find(params[:id])
+     @address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
   end
 
 private
 
   def address_params
-      params.permit(:customer_id, :postal_code, :address, :name)
+      params.require(:address).permit(:customer_id, :postal_code, :address, :name)
   end
 
 end
