19dcbbb1d9bd2489a4c2be7e4265304dea5987a1
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-04-28 15:39:26 +0000

    [Update] customerレイアウトの修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,6 +2,7 @@ class Public::CartItemsController < ApplicationController
    def index
     @total = 0
     @cart_items = CartItem.all
+
    end
 
   def new
@@ -9,23 +10,30 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_items = CartItem.new(cart_item_params)
-    @cart_items.customer_id = current_customer.id
 
-   if  @cart_items.save
-      flash[:notice] = '商品が追加されました。'
-      redirect_to cart_items_path
-   else
-      flash[:alert] = '商品の追加に失敗しました。'
-      redirect_to cart_items_path
-   end
+    @cart_items = current_customer.cart_items.all
+    if cart_item_params[:amount] != ""
+      if @cart_items.any? { |cart_item| cart_item.item_id == params[:item_id].to_i }
+        @cart_items_already = CartItem.find_by(item_id: params[:item_id].to_i)
+        @cart_items_already.amount += params[:amount].to_i
+        @cart_items_already.save
+        flash[:success] = "カートに商品を入れました"
+        redirect_to cart_items_path
+      else
+        @cart_items = CartItem.new(cart_item_params)
+        @cart_items.customer_id = current_customer.id
+        @cart_items.save
+        flash[:notice] = '商品が追加されました。'
+        redirect_to cart_items_path
+      end
+    end
+
   end
 
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
     redirect_to cart_items_path
-
   end
 
   def destroy
@@ -35,8 +43,8 @@ class Public::CartItemsController < ApplicationController
   end
 
   def all
-    cart_item = CartItem.all
-    cart_item.destroy_all
+    @cart_item = CartItem.all
+    @cart_item.destroy_all
     redirect_to cart_items_path
   end
 
